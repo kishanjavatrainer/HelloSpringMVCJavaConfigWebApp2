e582383d1275658604558600026c7da8937db124
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-07-01 18:13:45 +0530

    HelloSpringMVCJavaConfigWebApp2 source code

---
+++ HelloSpringMVCWebApp/src/com/infotech/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.infotech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages={"com.infotech.controller","com.infotech.service"})
+public class WebConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
